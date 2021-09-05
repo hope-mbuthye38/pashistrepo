b57d2876c1b7100fd2d6b64e810b1f7b8283e24a
hopembuthye38 <hope.mbuthye.moringaschool.com>
2021-09-06 02:28:44 +0300

    user test

---
+++ user_test.py
@@ -0,0 +1,9 @@
+import unittest #Importing the unittest module
+from user import user #Importing the user class
+class Testuser (unittest.Testcase):
+    '''
+    Test class that defines test cases for the user class behaviours .
+    Args:
+        unittest.Testcase: Testcase class that helps in creating test cases
+
+    '''
