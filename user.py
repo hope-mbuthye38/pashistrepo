ca7a05ce56943a266b9d672d280cbc362d6dd482
hopembuthye38 <hope.mbuthye.moringaschool.com>
2021-09-06 01:24:56 +0300

    init statements

--- user.py
+++ user.py
@@ -3,3 +3,12 @@ class User:
     Class that generates new instances for users
     """
     pass
+    
+    def__init__(self,first_name,last_name,telephone_number,email):
+    '''
+     _ init _ method that helps us define properties for our objects
+     first _name : New user first name.
+     last_name : New user last name.
+     telephone number : New user telephone number.
+     email : New user email address.
+    '''
