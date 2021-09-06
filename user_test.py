c13638ea8b37ae9f4f5a5fcdf29280cf7966ca6e
hopembuthye38 <hope.mbuthye.moringaschool.com>
2021-09-06 09:47:37 +0300

    error test run

--- user_test.py
+++ user_test.py
@@ -1,9 +1,37 @@
 import unittest #Importing the unittest module
-from user import user #Importing the user class
-class Testuser (unittest.Testcase):
+from user import User #Importing the user class
+
+class TestUser(unittest.TestCase):
     '''
     Test class that defines test cases for the user class behaviours .
     Args:
         unittest.Testcase: Testcase class that helps in creating test cases
-
     '''
+    # Items up here .......
+    def setUp(self):
+        '''
+        set up method to run before each test cases.
+        '''
+        self.new_user = User("Hope","Mbuthye","078563368" "hope.mbuthye21@gmail.com")                                                                           
+        # create contact object                 
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.first_name,"Hope")
+        self.assertEqual(self.new_user.last_name,"Mbuthye")
+        self.assertEqual(self.new_user.telephone_number,"078563368")
+        self.assertEqual(self.new_user.email,"hope.mbuthye21@gmail.com")
+    
+
+    def test_save_contact(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user() # saving the new contact
+        self.assertEqual(len(User.user_list),1)
+
+if __name__ ==  '__main__':
+    unittest.main()
