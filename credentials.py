a3457a46be7b8c370c2a533196f000c29a1f99c6
hopembuthye38 <hope.mbuthye.moringaschool.com>
2021-09-09 12:22:23 +0300

    password commmands

--- credentials.py
+++ credentials.py
@@ -1,3 +1,5 @@
+import string
+import random
 class Credentials :
     '''
     Class that generates new instances credentials
@@ -70,3 +72,10 @@ class Credentials :
         method that return the credentials list
         '''
         return cls.credentials_list
+
+    def producePassword(stringLength=8):
+        '''
+        method that generates a random password of string of digits,letters and special characters
+        '''
+        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "!@#><*&^"
+        return ''.join(random.choice(password) for i in range(stringLength))
